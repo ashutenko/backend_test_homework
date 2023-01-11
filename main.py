6280c021898d954fc012f9fcf1c56144eaac6221
Александр Шутенко <ashutenko@mail.ru>
2023-01-11 10:29:52 +0300

    Add new file

---
+++ main.py
@@ -0,0 +1,33 @@
+# character_creation_module/main.py
+
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+
+
+def attack(char_name: str, char_class: str) -> str:
+    """Функция считает аттаку."""
+    ...
+
+
+def defence(char_name: str, char_class: str) -> str:
+    """Функция считает защиту."""
+    ...
+
+
+def special(char_name: str, char_class: str) -> str:
+    """Функция считает навыки."""
+    ...
+
+
+def start_training(char_name: str, char_class: str) -> str:
+    """Функция считает подготовку."""
+    ...
+
+
+def choice_char_class() -> str:
+    """Функция выбора."""
+    ...
+
+
+# Докстринги к коду:
